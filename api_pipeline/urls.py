a46863219a7b6bce2acf26802732268d36634d05
Teemerte Teccola <teemerte.teccola@digital.trade.gov.uk>
2024-09-16 14:31:27 +0100

    update the path and inrouduce Hawk authentication to api/pipeline

--- api_pipeline/urls.py
+++ api_pipeline/urls.py
@@ -5,7 +5,7 @@ from . import views as views
 
 urlpatterns = [
     path(
-        "<path:path>/user-inline-feedback-survey",
+        "user-inline-feedback-survey/",
         views.UserInlineFeedbackSurveyViewSet.as_view({"get": "list"}),
         name="user-inline-feedback-survey",
     ),
--- api_pipeline/views.py
+++ api_pipeline/views.py
@@ -4,12 +4,14 @@ from rest_framework.pagination import PageNumberPagination
 from .serializers import UserInlineFeedbackSurveySerializer
 from inline_feedback.models import UserInlineFeedbackSurvey
 
+from article.hawk import HawkAuthentication
+
 
 class UserInlineFeedbackSurveyViewSet(viewsets.ModelViewSet):
     """
     API endpoint to capture user inline feedback survey for ingestion by data flow
     """
-
+    authentication_classes = (HawkAuthentication,)
     queryset = UserInlineFeedbackSurvey.objects.all()
     serializer_class = UserInlineFeedbackSurveySerializer
     pagination_class = PageNumberPagination
